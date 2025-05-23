886c5c564095c22d6ca70657071f66c6487f38dd
Edgar Hernandez <ehernandez@chh.s-riko.com>
2025-05-22 20:51:10 -0600

    Categories

--- DWShop.Application/Responses/Catalog/CatalogResponse.cs
+++ DWShop.Application/Responses/Catalog/CatalogResponse.cs
@@ -1,11 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel.DataAnnotations.Schema;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace DWShop.Application.Responses.Catalog
+﻿namespace DWShop.Application.Responses.Catalog
 {
 	public class CatalogResponse
 	{
---
+++ DWShop.Application/Responses/Category/CategoryResponse.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DWShop.Application.Responses.Category
+{
+	public class CategoryResponse
+	{
+		public int Id { get; set; }
+		public string Name { get; set; }
+	}
+}
